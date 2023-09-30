e578f44decdfcc8531921046102aa32431859995
Hai Dinh NGUYEN <sandersonns@gmail.com>
2023-09-30 23:57:49 +0200

    Set up user authentication with Devise, allowing users to log in using either their email or username. Added some fields to the User model for their profile and validation for the username field.

---
+++ app/models/user.rb
@@ -0,0 +1,61 @@
+class User < ApplicationRecord
+  # Devise modules for authentication and registration
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:login]
+
+  # Attribute accessor for the virtual "login" field used for authentication
+  attr_accessor :login
+
+  # List of restricted usernames that users are not allowed to use
+  RESTRICTED_USERNAMES = %w(admin support help).freeze
+
+  # Validations
+
+  # Ensure the username is unique, case insensitive
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false }, 
+                       length: { minimum: 6, maximum: 50 }
+                       
+  # Optional fields with length validations
+  validates :first_name, length: { minimum: 1, maximum: 50 }, allow_blank: true
+  validates :last_name, length: { minimum: 1, maximum: 50 }, allow_blank: true
+
+  # Validate email format
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Bio validation
+  validates :bio, length: { maximum: 500 }, allow_blank: true
+
+  # Custom validation to check restricted usernames
+  validate :username_not_restricted 
+
+  # Model associations
+  has_many :messages
+  has_many :marks
+  has_many :tokens
+
+  # Override Devise's method to allow authentication using either username or email
+  # This method is called by Devise to find the user in the database
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.strip.downcase }]).first
+  end
+
+  # Getter for the login attribute
+  def login
+    @login || self.username || self.email
+  end
+
+  private
+
+  # Custom validation to prevent the use of restricted usernames
+  def username_not_restricted
+    if RESTRICTED_USERNAMES.include?(username&.downcase)
+      errors.add(:username, "is restricted. Please choose another one.")
+    end
+  end
+end
---
+++ db/migrate/20230930181707_add_profile_and_username_to_users.rb
@@ -0,0 +1,10 @@
+class AddProfileAndUsernameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :bio, :text
+    add_column :users, :messages_privacy, :boolean
+  end
+end
